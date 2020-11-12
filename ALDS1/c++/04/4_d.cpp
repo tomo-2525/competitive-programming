e5b18dd680d637b8d80a0ed76f4d8a889b86b306
tomo-2525 <tomoblog.0205@gmail.com>
2020-11-12 23:14:25 +0900

    update

--- ALDS1/c++/04/4_d.cpp
+++ ALDS1/c++/04/4_d.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int n, min = 10000005, max = -10000005;
     long long sum = 0;
     cin >> n;
     int num[n];
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cin >> num[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
+        }
+    for (int i = 0; i < n; i++){
         if (min > num[i])
             min = num[i];
         if (max < num[i])
@@ -20,6 +17,5 @@ int main()
         sum += num[i];
     }
     cout << min << " " << max << " " << sum << endl;
-
     return 0;
 }
